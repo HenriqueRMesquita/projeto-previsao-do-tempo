c9ee75a6551f58916eeaeea2d0b8544c4439278e
HenriqueRMesquita <Henriquevest1@gmail.com>
2025-01-09 13:26:58 -0300

    Finalizado

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
-
-
 const key = "0d8799eae947b86063c2f8a1b0e19d82"
 
+function showdata(dados){
+    document.querySelector(".city").innerHTML= "Tempo em " + dados.name
+    document.querySelector(".celsius").innerHTML= Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".prevision-text").innerHTML= dados.weather[0].description
+    document.querySelector(".umi").innerHTML= "Umidade: " +  dados.main.humidity + "%"
+    document.querySelector(".prevision").src= `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+ async function seachcity(city){
 
-async function seachcity (city) {
-    const data = await fetch ("ttps://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key´)
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
 
+    showdata(dados)
 }
 
 function clickon () {
